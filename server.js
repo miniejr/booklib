3f9f1ff6d8940f11c87b2754b6a3e25dd7a5f0af
rizone <miniejr@gmail.com>
2021-02-19 18:34:40 +0700

    innital author

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     useUnifiedTopology: true
@@ -20,16 +21,18 @@ db.once('open',()=>{
 })
 
 const indexRouter = require('./routers/index');
+const authorRouter = require('./routers/authors');
 
 app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb',extended: false}))
 
 
 
 app.use('/',indexRouter);
-
+app.use('/authors',authorRouter);
 
 app.listen(process.env.PORT || 3000);
